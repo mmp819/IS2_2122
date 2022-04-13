aa441750c99f0cddf596dbb5d2db5f3940a210ac
Mario Martín <mmp819@alumnos.unican.es>
2022-04-13 13:18:39 +0200

    Añadir casos de prueba de caja negra a ListaOrdenadaAcotada. Corregir defectos encontrados en ListaOrdenadaAcotada.

---
+++ practica4/ListaOrdenadaAcotada/src/test/java/es/unican/is2/listaOrdenadaAcotada/ListaOrdenadaAcotadaTest.java
@@ -0,0 +1,170 @@
+package es.unican.is2.listaOrdenadaAcotada;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListaOrdenadaAcotadaTest {
+	
+	private ListaOrdenadaAcotada<Integer> sut;
+	
+	@Before
+	public void preparacion() {
+		sut = new ListaOrdenadaAcotada<Integer>();
+	}
+	
+	@Test
+	public void testConstructor() {
+		assertTrue(sut.size()==0);
+	}
+	
+	@Test
+	public void testAddYGet() {
+		try {
+			sut.get(0);
+			fail("Debería enviar IndexOutOfBoundsException");
+		} catch (IndexOutOfBoundsException e) {
+		}
+		
+		try {
+			sut.add(null);
+			fail("Debería enviar IllegalStateException.");
+		} catch (NullPointerException e) {
+		}
+		
+		try {
+			sut.add(1);
+			assertTrue(sut.get(0) == 1);
+		} catch (IndexOutOfBoundsException e) {
+			fail("Debería retornar correctamente 1 para el índice 0.");
+		} catch (IllegalStateException e) {
+			fail("Debería agregar correctamente el elemento.");
+		}
+		
+		try {
+			sut.add(2);
+			assertTrue(sut.get(1) == 2);
+			assertTrue(sut.get(0) == 1);
+		} catch (IndexOutOfBoundsException e) {
+			fail("Debería retornar correctamente 2 para ID 1 y 1 para ID 0.");
+		} catch (IllegalStateException e) {
+			fail("Debería agregar correctamente el elemento.");
+		}
+		
+		try {
+			sut.add(4);
+			sut.add(5);
+			sut.add(3);
+			sut.add(6);
+			sut.add(7);
+			sut.add(10);
+			sut.add(8);
+			sut.add(9);
+			assertTrue(sut.get(2) == 3);
+			assertTrue(sut.get(9) == 10);
+			assertTrue(sut.get(0) == 1);
+		} catch (IndexOutOfBoundsException e) {
+			fail ("Debería retornar correctamente índices en los extremos y en el centro.");
+		} catch (IllegalStateException e) {
+			fail ("No se debería haber sobrepasado el máximo de la lista.");
+		}
+		
+		try {
+			sut.add(11);
+			fail("Debería haber lanzado IllegalStateException.");
+		} catch (IllegalStateException e) {
+		}
+		
+		try {
+			sut.get(-1);
+			fail("Debería haber lanzado IndexOutOfBoundsException.");
+		} catch (IndexOutOfBoundsException e) {
+		}
+		
+		try {
+			sut.get(10);
+			fail("Debería haber lanzado IndexOutOfBoundsException.");
+		} catch (IndexOutOfBoundsException e) {
+		}
+	}
+	
+	@Test
+	public void testRemove() {
+		sut.add(1);
+		sut.add(2);
+		sut.add(4);
+		sut.add(5);
+		sut.add(3);
+		sut.add(6);
+		sut.add(7);
+		sut.add(10);
+		sut.add(8);
+		sut.add(9);
+		try {
+			sut.remove(-1);
+			fail("Debería haber lanzado IndexOutOfBoundsException.");
+		} catch (IndexOutOfBoundsException e) {
+		}
+		
+		try {
+			sut.remove(10);
+			fail("Debería haber lanzado IndexOutOfBoundsException.");
+		} catch (IndexOutOfBoundsException e) {
+		}
+		
+		try {
+			assertTrue(sut.remove(0) == 1);
+			assertTrue(sut.size() == 9);
+			assertTrue(sut.remove(3) == 5);
+			assertTrue(sut.size() == 8);
+			assertTrue(sut.remove(7) == 10);
+			assertTrue(sut.size() == 7);
+		} catch (IndexOutOfBoundsException e) {
+			fail("No deberían existir fallos en los índices.");
+		}
+	}
+	
+	@Test
+	public void testClearYSize() {
+		sut.add(2);
+		sut.add(4);
+		sut.add(3);
+		sut.add(6);
+		sut.add(7);
+		sut.add(8);
+		sut.add(9);
+		assertTrue(sut.size() == 7);
+		
+		sut.add(1);
+		sut.add(5);
+		sut.add(10);
+		
+		assertTrue(sut.size() == 10);
+		
+		sut.clear();
+		assertTrue(sut.size() == 0);
+		
+		sut.clear();
+		assertTrue(sut.size() == 0);
+		
+		sut.add(1);
+		assertTrue(sut.size() == 1);
+		
+		sut.clear();
+		assertTrue(sut.size() == 0);
+		
+		sut.add(1);
+		assertTrue(sut.size() == 1);
+		
+		sut.add(2);
+		sut.add(3);
+		sut.add(4);
+		sut.add(5);
+		assertTrue(sut.size() == 5);
+		
+		sut.clear();
+		assertTrue(sut.size() == 0);
+	}
+}  
