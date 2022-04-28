b1a7cf03ec3f1185ef94afa33cf0e6f12dd9b86d
Mario Martín <mmp819@alumnos.unican.es>
2022-04-28 13:30:05 +0200

    Añadir WMC sin refactorizar

--- practica5/GestionTiendaNoRefactorizada/src/main/java/es/unican/is2/gestionTienda/gui/GestionComisiones.java
+++ practica5/GestionTiendaNoRefactorizada/src/main/java/es/unican/is2/gestionTienda/gui/GestionComisiones.java
@@ -20,7 +20,7 @@ public class GestionComisiones {
 	/**
 	 * Programa principal basado en menu
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) { // WMC + 1 
 		// opciones del menu
 		final int NUEVA_VENTA = 0, VENDEDOR_DEL_MES = 1, VENDEDORES = 2;
 
@@ -43,12 +43,12 @@ public class GestionComisiones {
 		int opcion;
 
 		// lazo de espera de comandos del usuario
-		while (true) {
+		while (true) { // WMC + 1
 			opcion = menu.leeOpcion();
 
 			// realiza las acciones dependiendo de la opcion elegida
 			switch (opcion) {
-			case NUEVA_VENTA:
+			case NUEVA_VENTA: // WMC + 1 
 				lect = new Lectura("Datos Venta");
 				lect.creaEntrada("Id Vendedor", "");
 				lect.creaEntrada("Importe", "");
@@ -56,7 +56,7 @@ public class GestionComisiones {
 				dni = lect.leeString("Id Vendedor");
 				double importe = lect.leeDouble("Importe");
 				try {
-					if (!tienda.anhadeVenta(dni, importe)) {
+					if (!tienda.anhadeVenta(dni, importe)) { // WMC + 1
 						mensaje("ERROR", "El vendedor no existe");
 					}
 				} catch (IOException e) {
@@ -64,36 +64,36 @@ public class GestionComisiones {
 				}
 				break;
 
-			case VENDEDOR_DEL_MES:
+			case VENDEDOR_DEL_MES: // WMC + 1
 
 				vendedores = tienda.vendedores();
 				resultado = new LinkedList<Vendedor>();
 				double maxVentas = 0.0;
-				for (Vendedor v : vendedores) {
-					if (v.getTotalVentas() > maxVentas) {
+				for (Vendedor v : vendedores) { // WMC + 1
+					if (v.getTotalVentas() > maxVentas) { // WMC + 1
 						maxVentas = v.getTotalVentas();
 						resultado.clear();
 						resultado.add(v);
-					} else if (v.getTotalVentas() == maxVentas) {
+					} else if (v.getTotalVentas() == maxVentas) { // WMC + 1
 						resultado.add(v);
 					}
 				}
 
 				msj = "";
-				for (Vendedor vn : resultado) {
+				for (Vendedor vn : resultado) { // WMC + 1 
 					msj += vn.getNombre() + "\n";
 				}
 				mensaje("VENDEDORES DEL MES", msj);
 				break;
 
 			
-		case VENDEDORES:
+		case VENDEDORES: // WMC + 1
 
 			vendedores = tienda.vendedores();
 			System.out.println(vendedores.size());
 			Collections.sort(vendedores, new ComparadorVendedorVentas());			
 			msj = "";
-			for (Vendedor vn : vendedores) {
+			for (Vendedor vn : vendedores) { // WMC + 1
 				msj += vn.getNombre() + " " + vn.getTotalVentas() + "\n";
 			}
 			mensaje("VENDEDORES", msj);
@@ -107,18 +107,18 @@ public class GestionComisiones {
 	 * @param titulo Titulo de la ventana
 	 * @param txt Texto contenido en la ventana
 	 */
-	private static void mensaje(String titulo, String txt) {
+	private static void mensaje(String titulo, String txt) { // WMC + 1
 		Mensaje msj = new Mensaje(titulo);
 		msj.escribe(txt);
 
 	}
 	
-	public static class ComparadorVendedorVentas implements Comparator<Vendedor>  {
+	public static class ComparadorVendedorVentas implements Comparator<Vendedor>  { // WMC + 1
 
-		public int compare(Vendedor o1, Vendedor o2) {
-			if (o1.getTotalVentas()>o2.getTotalVentas())
+		public int compare(Vendedor o1, Vendedor o2) { // WMC + 1
+			if (o1.getTotalVentas()>o2.getTotalVentas()) // WMC + 1
 				return -1;
-			else if (o1.getTotalVentas()<o2.getTotalVentas())
+			else if (o1.getTotalVentas()<o2.getTotalVentas()) // WMC + 1
 				return 1;
 			return 0;
 		}
--- practica5/GestionTiendaNoRefactorizada/src/main/java/es/unican/is2/gestionTienda/vendedorEnPracticas.java
+++ practica5/GestionTiendaNoRefactorizada/src/main/java/es/unican/is2/gestionTienda/vendedorEnPracticas.java
@@ -10,18 +10,18 @@ public class vendedorEnPracticas extends Vendedor {
 	 * @param nombre
 	 * @param dni
 	 */
-	public vendedorEnPracticas(String nombre, String id, String dni) {
+	public vendedorEnPracticas(String nombre, String id, String dni) { // WMC + 1
 		super(nombre, id);
 		this.dni= dni;
 	}
 	
-	public String getDni() {
+	public String getDni() { // WMC + 1
 		return dni;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof vendedorEnPracticas)) 
+		if (!(obj instanceof vendedorEnPracticas)) // WMC + 1
 			return false;
 		vendedorEnPracticas v = (vendedorEnPracticas) obj;
 		return (v.getId().equals(getId()) && v.getDni().equals(getDni()));
